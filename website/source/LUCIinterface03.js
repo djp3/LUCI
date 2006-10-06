1525a707e8f128f8e3df5038a9ad1175e3e08cd1
Don Patterson <d_j_p_3@djp3.net>
2006-10-06 21:16:53 +0000

    Tagging v1.1

--- website/source/LUCIinterface03.js
+++ website/source/LUCIinterface03.js
@@ -1,5 +1,37 @@
-function jsUpdateAddress(x) {
+//This function is used by ActionScript to see if it's launched from a browser
+function jsAvailable() {
+	return true;
+}
+
+function jsSetLocation(x) {
     window.location.hash = x;
+}
+
+function jsDebug(x) {
+   	var foo = document.getElementById("form");
+    var child = document.createElement("div");
+	child.innerHTML = "Debug: "+x;
+	foo.appendChild(child);
+}
+
+function jsUpdateLocation(x,level) {
+	var newThing = "";
+	var y = window.location.hash.substring(1);
+
+	jsDebug("jsUpdateLocation called with: "+x + " " + level);
+	while(level > 1){
+		end = y.indexOf("&");
+		if(end != -1){
+			newThing = newThing + y.substring(0,end) + "&"; 
+			y = y.substring(end+1);
+			level = level -1;
+		}
+		else{
+			newThing = newThing + y + "&";
+			level = 1;
+		}
+	}
+    window.location.hash = newThing + x;
 }
 
 function checkURLParameters() {
@@ -7,8 +39,14 @@ function checkURLParameters() {
 	return query;
 }
 
+function jsStartFromActionScript(){
+	jsDebug("jsStartFromActionScript called");
+	callMovie().animateOpen(checkURLParameters());
+	//setTimeout("callMovie().animateOpen(checkURLParameters());",500);
+}
+
 window.onload = function(){
+	jsDebug("Onload called");
 	//callMovie().animateOpen(checkURLParameters());
-	setTimeout("callMovie().animateOpen(checkURLParameters());",500);
 }
 
