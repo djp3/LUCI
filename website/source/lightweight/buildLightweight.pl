3f180cbf4bbd7fa2bbe78c7fe62ce956403b9e08
Don Patterson <d_j_p_3@djp3.net>
2006-11-21 15:53:13 +0000

    Updating lightweight page

--- website/source/lightweight/buildLightweight.pl
+++ website/source/lightweight/buildLightweight.pl
@@ -10,89 +10,108 @@ my $doc = $parser->parsefile($file);
 
 sub outputHTMLHeader()
 {
-	my $filepath=$_[0];
-	my $deepLink=$_[1];
-	print OUTFILE "<html>\n";
-	print OUTFILE "<head>\n";
-	print OUTFILE "<title>LUCI Light Weight</title>\n";
-	print OUTFILE "<link rel=\"stylesheet\" href=\"http://luci.ics.uci.edu/blog/styles-site.css\" type=\"text/css\" />\n";
-	print OUTFILE "</head>\n";
-	print OUTFILE "<body>\n";
-	print OUTFILE "<div class=\"container\">\n";
-	print OUTFILE "<div class=\"banner\">\n";
-	print OUTFILE "<div class=\"bannerblock\">\n";
-	print OUTFILE "<table>\n";
-	print OUTFILE "<tr>\n";
-	print OUTFILE "<td style=\"padding:20px;margins:20px\">\n";
-	print OUTFILE "<img src=\"".$filepath."LUCIhorzTight.jpg\" alt=\"LUCI logo\"/>\n";
-	print OUTFILE "</td>\n";
-	print OUTFILE "<td>\n";
-	print OUTFILE "<p><a href=\"http://luci.ics.uci.edu/#".$deepLink."\">Click Here to Switch to Interactive FlashVersion</a></p>\n";
-	print OUTFILE "</td>\n";
-	print OUTFILE "</tr>\n";
-	print OUTFILE "</table>\n";
-	print OUTFILE "</div>\n";
-	print OUTFILE "</div>\n";
-	print OUTFILE "</div>\n";
+	my $OUTFILE=$_[0];
+	my $filepath=$_[1];
+	my $deepLink=$_[2];
+	print $OUTFILE "<html>\n";
+	print $OUTFILE "<head>\n";
+	print $OUTFILE "<title>LUCI Light Weight</title>\n";
+	print $OUTFILE "<link rel=\"stylesheet\" href=\"http://luci.ics.uci.edu/blog/styles-site.css\" type=\"text/css\" />\n";
+	print $OUTFILE "</head>\n";
+	print $OUTFILE "<body>\n";
+	print $OUTFILE "<div class=\"container\">\n";
+	print $OUTFILE "<div class=\"banner\">\n";
+	print $OUTFILE "<div class=\"bannerblock\">\n";
+	print $OUTFILE "<table>\n";
+	print $OUTFILE "<tr>\n";
+	print $OUTFILE "<td style=\"padding:20px;margins:20px\">\n";
+	print $OUTFILE "<img src=\"".$filepath."LUCIhorzTight.jpg\" alt=\"LUCI logo\"/>\n";
+	print $OUTFILE "</td>\n";
+	print $OUTFILE "<td>\n";
+	print $OUTFILE "<p><a href=\"http://luci.ics.uci.edu/#".$deepLink."\">Click Here to Switch to Interactive FlashVersion</a></p>\n";
+	print $OUTFILE "</td>\n";
+	print $OUTFILE "</tr>\n";
+	print $OUTFILE "</table>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
 }
 
 sub outputHTMLFooter()
 {
-	print OUTFILE "</body>\n";
-	print OUTFILE "</html>\n";
+	my $OUTFILE=$_[0];
+	print $OUTFILE "</body>\n";
+	print $OUTFILE "</html>\n";
 }
 
-my @deepLinkList;
+sub makeSubpage()
+{
+	my $root = $_[0];
+	my $deepLink = ($root->getElementsByTagName('deepLink')->item(0)->getFirstChild->getNodeValue);
+	my $template = ($root->getElementsByTagName('template')->item(0));
+	my $templateTitle = ($template->getElementsByTagName('title')->item(0)->getFirstChild->getNodeValue);
 
-open(OUTFILE, "> index.html")||die;
-&outputHTMLHeader("","");
+	mkdir $deepLink;
+	my $OUTFILE;
+	open($OUTFILE, "> $deepLink/index.html")||die;
 
-print OUTFILE "<div class=\"container\">\n";
-print OUTFILE "<div class=\"content\">\n";
+	&outputHTMLHeader($OUTFILE,"../",$deepLink);
+
+	print $OUTFILE "<div class=\"container\">\n";
+	print $OUTFILE "<div class=\"content\">\n";
+	print $OUTFILE "<a href=\"..\\index.html\">UP</a>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "<div class=\"content\">\n";
+	print $OUTFILE "<p>$deepLink</p>\n";
+	print $OUTFILE "<p>$templateTitle</p>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
+	&outputHTMLFooter($OUTFILE);
+	close($OUTFILE);
+}
+
+
+my $OUTFILE;
+open($OUTFILE, "> index.html")||die;
+&outputHTMLHeader($OUTFILE,"","");
+
+print $OUTFILE "<div class=\"container\">\n";
+print $OUTFILE "<div class=\"content\">\n";
 foreach my $menuItems ($doc->getElementsByTagName('menuItem')){
-	my $text = $menuItems->getElementsByTagName('title')->item(0)->getFirstChild->getNodeValue;
-
 	my @indent = $menuItems->getElementsByTagName('indent');
 	my $indent=0;
 	if($#indent != -1){
 		$indent= $menuItems->getElementsByTagName('indent')->item(0)->getFirstChild->getNodeValue;
 	}
+	my @title = $menuItems->getElementsByTagName('title');
+	my $title=0;
+	if($#title != -1){
+		$title= $menuItems->getElementsByTagName('title')->item(0)->getFirstChild->getNodeValue;
+	}
+
 	for (my $i = 0; $i< $indent; $i++){
-		print OUTFILE "+++++";
+		print $OUTFILE "+++++";
 	}
 
-	my $deepLinked = 0;
-	foreach my $deepLinks ($menuItems->getElementsByTagName('deepLink')){
-		push @deepLinkList, $deepLinks->getFirstChild->getNodeValue;
-		print OUTFILE "<a href=\"".$deepLinks->getFirstChild->getNodeValue."/index.html\">$text</a>\n";
-		$deepLinked = 1;
+	#Check to see about making a subpage
+	my @clickable = $menuItems->getElementsByTagName('clickable');
+	if($#clickable != -1){
+		&makeSubpage($clickable[0]);
+
+		foreach my $deepLinks ($clickable[0]->getElementsByTagName('deepLink')){
+			print $OUTFILE "<a href=\"".$deepLinks->getFirstChild->getNodeValue."/index.html\">$title</a>\n";
+		}
 	}
-	if ($deepLinked == 0){
-		print OUTFILE $text."\n";
+	else{
+		print $OUTFILE $title."\n";
 	}
-	print OUTFILE "<br/>\n";
+	print $OUTFILE "<br/>\n";
 }
-print OUTFILE "</div>\n";
-print OUTFILE "</div>\n";
-&outputHTMLFooter();
+print $OUTFILE "</div>\n";
+print $OUTFILE "</div>\n";
+&outputHTMLFooter($OUTFILE);
 
-close(OUTFILE);
-
-foreach my $deepLinks (@deepLinkList){
-	mkdir $deepLinks;
-	open(OUTFILE, "> $deepLinks/index.html")||die;
-	&outputHTMLHeader("../",$deepLinks);
-	print OUTFILE "<div class=\"container\">\n";
-	print OUTFILE "<div class=\"content\">\n";
-	print OUTFILE "<a href=\"..\\index.html\">UP</a>\n";
-	print OUTFILE "</div>\n";
-	print OUTFILE "<div class=\"content\">\n";
-	print OUTFILE "<p>$deepLinks</p>\n";
-	print OUTFILE "</div>\n";
-	print OUTFILE "</div>\n";
-	&outputHTMLFooter();
-	close(OUTFILE);
-}
+close($OUTFILE);
 
 
 
