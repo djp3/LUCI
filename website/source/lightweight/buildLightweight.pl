fbbda7e219bb59ce261ca8be1ae16eac4b10a684
Don Patterson <d_j_p_3@djp3.net>
2006-11-27 19:46:55 +0000

    added urchin tracker

--- website/source/lightweight/buildLightweight.pl
+++ website/source/lightweight/buildLightweight.pl
@@ -40,13 +40,53 @@ sub outputHTMLHeader()
 sub outputHTMLFooter()
 {
 	my $OUTFILE=$_[0];
+	print $OUTFILE "<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\"></script>\n";
+	print $OUTFILE "<script type=\"text/javascript\">\n";
+	print $OUTFILE "\tuacct = \"UA-338915-2\"\n";
+	print $OUTFILE "\turchinTracker();\n";
+	print $OUTFILE "</script>\n";
 	print $OUTFILE "</body>\n";
 	print $OUTFILE "</html>\n";
+}
+sub makeTemplateAPage()
+{
+	my $OUTFILE = $_[0];
+	my $url = $_[1];
+	my $title = $_[2];
+
+	print $OUTFILE "<div class=\"container\">\n";
+	print $OUTFILE "<div class=\"content\">\n";
+	print $OUTFILE "<a href=\"..\\index.html\">UP</a>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
+
+	print $OUTFILE "<div class=\"container\">\n";
+	print $OUTFILE "<div class=\"content\">\n";
+	print $OUTFILE $title."\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
+
+	print $OUTFILE "<div class=\"container\">\n";
+	print $OUTFILE "<div class=\"content\">\n";
+	print $OUTFILE "<div style=\"text-align:left;\">\n";
+
+	my $TEMPLATE;
+	open ($TEMPLATE,"< ../".$url) || die;
+	while(<$TEMPLATE>){
+			s/asfunction:_root.jumpToURLNewWindow,//g;
+			print $OUTFILE $_;
+	}
+	close($TEMPLATE);
+
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
+	print $OUTFILE "</div>\n";
 }
 
 sub makeSubpage()
 {
-	my $root = $_[0];
+	my $title = $_[0];
+	my $root = $_[1];
 	my $deepLink = ($root->getElementsByTagName('deepLink')->item(0)->getFirstChild->getNodeValue);
 	my $template = ($root->getElementsByTagName('template')->item(0));
 	my $templateTitle = ($template->getElementsByTagName('title')->item(0)->getFirstChild->getNodeValue);
@@ -55,19 +95,30 @@ sub makeSubpage()
 	my $OUTFILE;
 	open($OUTFILE, "> $deepLink/index.html")||die;
 
-	&outputHTMLHeader($OUTFILE,"../",$deepLink);
+	if($template->getElementsByTagName('type')->item(0)->getFirstChild->getNodeValue eq "A"){
+		&outputHTMLHeader($OUTFILE,"../",$deepLink);
+		&makeTemplateAPage($OUTFILE,$root->getElementsByTagName('template')->item(0)->getElementsByTagName('url')->item(0)->getFirstChild->getNodeValue,$title.":".$templateTitle);
+		&outputHTMLFooter($OUTFILE);
+	}
+	else{
 
-	print $OUTFILE "<div class=\"container\">\n";
-	print $OUTFILE "<div class=\"content\">\n";
-	print $OUTFILE "<a href=\"..\\index.html\">UP</a>\n";
-	print $OUTFILE "</div>\n";
-	print $OUTFILE "<div class=\"content\">\n";
-	print $OUTFILE "<p>$deepLink</p>\n";
-	print $OUTFILE "<p>$templateTitle</p>\n";
-	print $OUTFILE "</div>\n";
-	print $OUTFILE "</div>\n";
-	&outputHTMLFooter($OUTFILE);
-	close($OUTFILE);
+		&outputHTMLHeader($OUTFILE,"../",$deepLink);
+
+		print $OUTFILE "<div class=\"container\">\n";
+		print $OUTFILE "<div class=\"content\">\n";
+		print $OUTFILE "<a href=\"..\\index.html\">UP</a>\n";
+		print $OUTFILE "</div>\n";
+		print $OUTFILE "<div class=\"content\">\n";
+		print $OUTFILE "<p>This template is not coded for lightweight page yet: ".$template->getElementsByTagName('type')->item(0)->getFirstChild->getNodeValue."</p>\n";
+		print $OUTFILE "</div>\n";
+		print $OUTFILE "<div class=\"content\">\n";
+		print $OUTFILE "<p>$deepLink</p>\n";
+		print $OUTFILE "<p>$templateTitle</p>\n";
+		print $OUTFILE "</div>\n";
+		print $OUTFILE "</div>\n";
+		&outputHTMLFooter($OUTFILE);
+		close($OUTFILE);
+	}
 }
 
 
@@ -96,7 +147,7 @@ foreach my $menuItems ($doc->getElementsByTagName('menuItem')){
 	#Check to see about making a subpage
 	my @clickable = $menuItems->getElementsByTagName('clickable');
 	if($#clickable != -1){
-		&makeSubpage($clickable[0]);
+		&makeSubpage($title,$clickable[0]);
 
 		foreach my $deepLinks ($clickable[0]->getElementsByTagName('deepLink')){
 			print $OUTFILE "<a href=\"".$deepLinks->getFirstChild->getNodeValue."/index.html\">$title</a>\n";
