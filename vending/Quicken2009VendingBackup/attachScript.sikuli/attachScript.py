29f65fed1b9ae64c80e29a537aea7eb55b6d4e10
Don Patterson <d_j_p_3@djp3.net>
2011-08-03 18:21:25 +0000

    Before changing my interests

---
+++ vending/Quicken2009VendingBackup/attachScript.sikuli/attachScript.py
@@ -0,0 +1,12 @@
+click(Pattern("DeletzAttadi.png").similar(0.91).targetOffset(20,3))
+click(Pattern("DeletzAttadi.png").similar(0.91).targetOffset(20,3))
+click("ChedxRecevfl.png")
+click("maqehumcIile.png")
+click("sarndleciP9.png")
+click("uu.png")
+click("1305647456334.png")
+click("1IQ.png")
+
+
+
+
